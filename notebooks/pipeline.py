bd1bf9c8539eb18dd90b7558dc85950a07fa92f1
EvangMM <matteomartini92@yahoo.it>
2024-05-09 13:50:38 +0200

    up

--- notebooks/pipeline.py
+++ notebooks/pipeline.py
@@ -7,24 +7,24 @@ def myhandler(di_key):
             name="step1",
             function="mlrun-downloader",
             action="job",
-            inputs=[{"url": di_key}],
-            outputs=[{"dataset": "dataset"}],
+            inputs={"url": di_key},
+            outputs={"dataset": "dataset"},
         )
 
         s2_1 = pc.step(
             name="step2.1",
             function="mlrun-process-spire",
             action="job",
-            inputs=[{"di": s1.outputs["dataset"]}],
-            outputs=[{"dataset-spire": "dataset-spire"}],
+            inputs={"di": s1.outputs["dataset"]},
+            outputs={"dataset-spire": "dataset-spire"},
         )
 
         s2_2 = pc.step(
             name="step2.2",
             function="mlrun-process-measure",
             action="job",
-            inputs=[{"di": s1.outputs["dataset"]}],
-            outputs=[{"dataset-measures": "dataset-measures"}],
+            inputs={"di": s1.outputs["dataset"]},
+            outputs={"dataset-measures": "dataset-measures"},
         )
-    
+
         return s2_1.outputs["dataset-spire"], s2_2.outputs["dataset-measures"]
